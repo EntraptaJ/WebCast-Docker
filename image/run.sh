2d3b1acddf66c9c7b4840b6cd9106d2d0d0c2953
PunditSharp <PunditSharp@users.noreply.github.com>
2017-07-19 22:51:35 -0700

    Update run.sh

--- image/run.sh
+++ image/run.sh
@@ -2,4 +2,4 @@
 
 # Start CastWebAPI
 ipaddr=$(ip route get 1 | awk '{print $NF;exit}')
-node /castwebapi/castWebApi.js --hostname=$ipaddr
+node /castwebapi/cast-web-api/castWebApi.js --hostname=$ipaddr
