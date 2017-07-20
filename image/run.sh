a7c2f267dc6f0f65a8afbd7688f48a731d95ee55
PunditSharp <PunditSharp@users.noreply.github.com>
2017-07-19 21:12:49 -0700

    Update run.sh

--- image/run.sh
+++ image/run.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-cd /root/.castwebapi
-
-env_file="/root/.castwebapi/.env"
-install_file="/root/.castwebapi/install.sh"
-package_file="/root/.castwebapi/package.json"
-plugin_folder="/root/.castwebapi/plugins"
-
-# Install plugins via package.json
-if [ -f "$package_file" ]
-then
-    echo "Installing plugins from $package_file."
-
-    npm install
-else
-    echo "$package_file not found."
-fi
-
-# Install plugins via install.sh
-if [ -f "$install_file" ]
-then
-    echo "Installing plugins from $install_file."
-
-    /bin/bash $install_file
-else
-    echo "$install_file not found."
-fi
-
 # Start CastWebAPI
 ip_addr = "$(ip route get 1 | awk '{print $NF;exit}')"
 node /castwebapi/castWebApi.js --hostname=$ip_addr
