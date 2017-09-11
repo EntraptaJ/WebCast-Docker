14247208d7946e709a02d667a883356fb5872527
Kristian Jones <kristianjonesg6@gmail.com>
2017-09-11 12:23:25 -0400

    Update run.sh

--- image/run.sh
+++ image/run.sh
@@ -5,5 +5,5 @@ npm install
 
 # Start CastWebAPI
 ipaddr=$(ip route get 1 | awk '{print $NF;exit}')
-#node /castwebapi/cast-web-api/castWebApi.js --hostname=$ipaddr
-node /castwebapi/smartthings-cast/app.js
+node /castwebapi/cast-web-api/castWebApi.js --hostname=$ipaddr
+
