2bf9d49f0739ed5e6db49076a1aa12800fd91aac
Kristian Jones <kristianjonesg6@gmail.com>
2017-10-01 12:58:04 -0500

    Update run.sh

--- image/run.sh
+++ image/run.sh
@@ -5,5 +5,5 @@ npm install
 
 # Start CastWebAPI
 ipaddr=$(ip route get 1 | awk '{print $NF;exit}')
-node /castwebapi/cast-web-api/castWebApi.js --hostname=$ipaddr
+node /castwebapi/castWeb/castWebApi.js --hostname=$ipaddr
 
